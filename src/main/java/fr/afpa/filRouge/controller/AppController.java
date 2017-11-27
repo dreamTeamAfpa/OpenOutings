4771bc8f395378405f12d7c56d2d129df633b0fd
Ludovic Castagnedoli <castaludo@gmail.com>
2017-11-27 17:03:42 +0100

    Ajout de la page CreateEvent.jsp

--- src/main/java/fr/afpa/filRouge/controller/AppController.java
+++ src/main/java/fr/afpa/filRouge/controller/AppController.java
@@ -14,4 +14,12 @@ public class AppController {
 	public String index(Model model) {
 		return "index";
 	}
+	@GetMapping("UserProfil")
+	public String UserProfil(Model model) {
+		return "UserProfil";
+	}
+	@GetMapping("VisitorProfil")
+	public String VisitorProfil(Model model) {
+		return "VisitorProfil";
+	}
 }
