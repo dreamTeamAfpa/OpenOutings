24d00b391c939c112a9b9eede0fa52fb622ace14
TITECA Raynald <ray_ex_59@hotmail.com>
2017-11-28 14:13:08 +0100

    correction bug conflit

--- src/main/java/fr/afpa/filRouge/model/Groups.java
+++ src/main/java/fr/afpa/filRouge/model/Groups.java
@@ -14,33 +14,36 @@ public class Groups {
 	int idGroup;
 	String nameGroup;
 	String descriptionGroup;
-	
-	
-	 @Id
-	    @GeneratedValue(strategy = GenerationType.AUTO)
-	 public int getIdGroup() {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public int getIdGroup() {
 		return idGroup;
-	 }
-	 
-	 public void setIdGroup(int idGroup) {
-		 this.idGroup = idGroup;
-	 }
+	}
+
+	public void setIdGroup(int idGroup) {
+		this.idGroup = idGroup;
+	}
+
 	public String getNameGroup() {
 		return nameGroup;
 	}
+
 	public void setNameGroup(String nameGroup) {
 		this.nameGroup = nameGroup;
 	}
-	
+
 	public String getDescriptionGroup() {
 		return descriptionGroup;
 	}
+
 	public void setDescriptionGroup(String descriptionGroup) {
 		this.descriptionGroup = descriptionGroup;
 	}
-	@OneToOne(mappedBy ="Administrator")
-	@JoinTable(name="User", joinColumns = @JoinColumn(name="id_person"))
-	public Set<Administrator>getAdministrator(){
+
+	@OneToOne(mappedBy = "Administrator")
+	@JoinTable(name = "User", joinColumns = @JoinColumn(name = "id_person"))
+	public Set<Administrator> getAdministrator() {
 		return administator;
 	}
 }
