8b5ca88d58fc51b8a60830269708ccb434e6765e
TITECA Raynald <ray_ex_59@hotmail.com>
2017-11-30 17:05:18 +0100

    correction syntax

--- src/main/java/fr/afpa/filRouge/controller/RechercheEventController.java
+++ src/main/java/fr/afpa/filRouge/controller/RechercheEventController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class RechcercheEventController {
+public class RechercheEventController {
 	
 	//affiche page rechercheEvent
-	@GetMapping("")
+	@GetMapping("rechercheEvent")
 	public String rechercheEvent(Model model) {
-		return "";
+		return "rechercheEvent";
 	}
 }
