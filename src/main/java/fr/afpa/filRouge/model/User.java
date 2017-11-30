c68e68154e567439bca8a2cb75ab0697a77efa15
Franck Descombes <franckdescombes@protonmail.com>
2017-11-30 11:54:18 +0100

    annotation user created

--- src/main/java/fr/afpa/filRouge/model/User.java
+++ src/main/java/fr/afpa/filRouge/model/User.java
@@ -2,20 +2,33 @@
 package fr.afpa.filRouge.model;
 
 import java.util.Calendar;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-
-
 
 @Entity
 @Table(name = "user")
 public class User extends Person {
+
+	private Set<Groupe> group;
 
 	public User(int idUser, String pseudoUser, String passwordUser, String firstNameUser, String lastNameUser,
 			Calendar dobUser, char genderUser, String emailUser, int phoneUser) {
 		super(idUser, pseudoUser, passwordUser, firstNameUser, lastNameUser, dobUser, genderUser, emailUser, phoneUser);
 	}
 
-}
+	@ManyToMany
+	@JoinTable(name = "group", joinColumns = @JoinColumn(name = "id_person"), inverseJoinColumns = @JoinColumn(name = "id_group))"))
+	public Set<Groupe> getGroup() {
+		return group;
+	}
 
+	public void setGroup(Set<Groupe> group) {
+		this.group = group;
+	}
+
+}
