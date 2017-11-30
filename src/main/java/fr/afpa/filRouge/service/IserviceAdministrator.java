945674537a4179d9972dc4092c61d4c53700f913
TITECA Raynald <ray_ex_59@hotmail.com>
2017-11-30 15:45:02 +0100

    rti - implémentation service et iservice Administator

--- src/main/java/fr/afpa/filRouge/service/IserviceAdministrator.java
+++ src/main/java/fr/afpa/filRouge/service/IserviceAdministrator.java
@@ -3,10 +3,27 @@
  */
 package fr.afpa.filRouge.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import fr.afpa.filRouge.model.Administrator;
+
 /**
  * @author PH GUIGUE
  *
  */
+@Service
 public interface IserviceAdministrator {
+
+	public List<Administrator> getAll();
+
+	public Administrator getOne(int id);
+
+	public void addAdministrator(Administrator administrator);
+
+	public void delAdministrator(Administrator administrator);
+
+	public void modifiedOne(Administrator administrator);
 
 }
--- src/main/java/fr/afpa/filRouge/service/ServiceAdministrator.java
+++ src/main/java/fr/afpa/filRouge/service/ServiceAdministrator.java
@@ -3,10 +3,56 @@
  */
 package fr.afpa.filRouge.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import fr.afpa.filRouge.model.Administrator;
+import fr.afpa.filRouge.repository.AdministratorRepository;
+
 /**
- * @author PH GUIGUE
+ * @author RTI
  *
  */
+@Service
 public class ServiceAdministrator implements IserviceAdministrator {
+	
+	private AdministratorRepository administratorRepository;
+	
+	public ServiceAdministrator(AdministratorRepository administratorRepository) {
+		this.administratorRepository = administratorRepository;
+	}
+
+	@Override
+	public List<Administrator> getAll() {
+		List<Administrator> administrators = administratorRepository.findAll();
+		return administrators;
+	}
+
+	@Override
+	public Administrator getOne(int id) {
+		Administrator administrator = administratorRepository.getOne(id);
+		return administrator;
+	}
+
+	@Override
+	public void addAdministrator(Administrator administrator) {
+		administratorRepository.save(administrator);
+	}
+
+	@Override
+	public void delAdministrator(Administrator administrator) {
+		administratorRepository.delete(administrator);
+	}
+
+	@Override
+	public void modifiedOne(Administrator administrator) {
+		administratorRepository.save(administrator);
+	}
+	
+	
+	
+	
+	
 
 }
