b0fbab2d3ef41f476c3903a9d56b0f3977669899
TITECA Raynald <ray_ex_59@hotmail.com>
2017-11-30 11:51:34 +0100

    ajout couche repository

--- src/main/java/fr/afpa/filRouge/repository/LocationRepository.java
+++ src/main/java/fr/afpa/filRouge/repository/LocationRepository.java
@@ -2,15 +2,17 @@
  * 
  */
 package fr.afpa.filRouge.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import fr.afpa.filRouge.model.Location;
+
 /**
  * @author 34011-79-09
  *
  */
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import fr.afpa.filRouge.model.Event;
-
 @Repository
-public interface EventRepository extends JpaRepository<Event,Integer>{
+public interface LocationRepository extends JpaRepository<Location, Integer> {
+
 }
