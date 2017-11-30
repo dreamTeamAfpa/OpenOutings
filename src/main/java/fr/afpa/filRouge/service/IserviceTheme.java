05b85b929ab8edaeb8bc9de29b9f6fc0a5fe8a3f
TITECA Raynald <ray_ex_59@hotmail.com>
2017-11-30 16:10:31 +0100

    Service Iservice theme

--- src/main/java/fr/afpa/filRouge/service/IserviceTheme.java
+++ src/main/java/fr/afpa/filRouge/service/IserviceTheme.java
@@ -3,10 +3,24 @@
  */
 package fr.afpa.filRouge.service;
 
+import java.util.List;
+
+import fr.afpa.filRouge.model.Theme;
+
 /**
- * @author PH GUIGUE
+ * @author RTI
  *
  */
 public interface IserviceTheme {
 
+	public List<Theme> getAll();
+	
+	public Theme getOne(String nameTheme);
+	
+	public void addTheme(Theme theme);
+	
+	public void delTheme(Theme theme);
+	
+	public void modifiedOne(Theme theme);
+	
 }
--- src/main/java/fr/afpa/filRouge/service/ServiceTheme.java
+++ src/main/java/fr/afpa/filRouge/service/ServiceTheme.java
@@ -3,10 +3,48 @@
  */
 package fr.afpa.filRouge.service;
 
+import java.util.List;
+
+import fr.afpa.filRouge.model.Theme;
+import fr.afpa.filRouge.repository.ThemeRepository;
+
 /**
  * @author PH GUIGUE
  *
  */
 public class ServiceTheme implements IserviceTheme {
+
+	private ThemeRepository themeRepository;
+
+	public ServiceTheme(ThemeRepository themeRepository) {
+		this.themeRepository = themeRepository;
+	}
+
+	@Override
+	public List<Theme> getAll() {
+		List<Theme> themes = themeRepository.findAll();
+		return themes;
+	}
+
+	@Override
+	public Theme getOne(String nameTheme) {
+		Theme theme = themeRepository.findOneByNameTheme(nameTheme);
+		return theme;
+	}
+
+	@Override
+	public void addTheme(Theme theme) {
+		themeRepository.save(theme);
+	}
+
+	@Override
+	public void delTheme(Theme theme) {
+		themeRepository.delete(theme);
+	}
+
+	@Override
+	public void modifiedOne(Theme theme) {
+		themeRepository.save(theme);
+	}
 
 }
