e296c68e6275a5796854da4aec502027d52d343e
Franck Descombes <franckdescombes@protonmail.com>
2017-11-30 13:32:48 +0100

    Theme class : annotation created

--- src/main/java/fr/afpa/filRouge/model/Theme.java
+++ src/main/java/fr/afpa/filRouge/model/Theme.java
@@ -3,25 +3,38 @@ package fr.afpa.filRouge.model;
 import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+/**
+ * @author FR DESCOMBES
+ *
+ */
 @Entity
 @Table(name = "Theme")
 public class Theme {
 
+	private String nameTheme;
 	private Set<Interest> interests;
 
-	/**
-	 * @return the interests
-	 */
+	@Id
+	public String getNameTheme() {
+		return nameTheme;
+	}
+
+	public void setNameTheme(String nameTheme) {
+		this.nameTheme = nameTheme;
+	}
+
+	@ManyToMany
+	@JoinTable(name = "interest", joinColumns = @JoinColumn(name = "name_theme"), inverseJoinColumns = @JoinColumn(name = "name_interest"))
 	public Set<Interest> getInterests() {
 		return interests;
 	}
 
-	/**
-	 * @param interests
-	 *            the interests to set
-	 */
 	public void setInterests(Set<Interest> interests) {
 		this.interests = interests;
 	}
