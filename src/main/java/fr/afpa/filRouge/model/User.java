bb8bc76f15ac37ba4f8147585f7710636044cee4
Franck Descombes <franckdescombes@protonmail.com>
2017-11-30 10:20:09 +0100

    update user class

---
+++ src/main/java/fr/afpa/filRouge/model/User.java
@@ -0,0 +1,31 @@
+package fr.afpa.filRouge.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int idUser;
+
+	/**
+	 * @return the idUser
+	 */
+	public int getIdUser() {
+		return idUser;
+	}
+
+	/**
+	 * @param idUser
+	 *            the idUser to set
+	 */
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+}
