571b1ee4cc5b142890f661926577a2c9a76d2385
Franck Descombes <franckdescombes@protonmail.com>
2017-11-30 16:00:17 +0100

    Minor modification on User class and Location Implementation of LocationRepository, ServiceLocation & IServiceLocation

--- src/main/java/fr/afpa/filRouge/model/Location.java
+++ src/main/java/fr/afpa/filRouge/model/Location.java
@@ -1,8 +1,6 @@
-
 package fr.afpa.filRouge.model;
 
 import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
@@ -18,57 +16,29 @@ public class Location {
 	@OneToMany(mappedBy = "location")
 	private Set<Location> location;
 
-	// CONSTRUCTOR USING ALL FIELDS
-	public Location(String postalCode, String nameLocation, Set<Location> location) {
-		super();
-		this.postalCode = postalCode;
-		this.nameLocation = nameLocation;
-		this.location = location;
-	}
-
 	// GETTERS & SETTERS
-	/**
-	 * @return the postalCode
-	 */
+
 	public String getPostalCode() {
 		return postalCode;
 	}
 
-	/**
-	 * @param postalCode
-	 *            the postalCode to set
-	 */
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
 	}
 
-	/**
-	 * @return the nameLocation
-	 */
 	public String getNameLocation() {
 		return nameLocation;
 	}
 
-	/**
-	 * @param nameLocation
-	 *            the nameLocation to set
-	 */
 	public void setNameLocation(String nameLocation) {
 		this.nameLocation = nameLocation;
 
 	}
 
-	/**
-	 * @return the location
-	 */
 	public Set<Location> getLocation() {
 		return location;
 	}
 
-	/**
-	 * @param location
-	 *            the location to set
-	 */
 	public void setLocation(Set<Location> location) {
 		this.location = location;
 	}
