601b9285d299d2707e49a68f0b90f47ecd5bf1c1
TITECA Raynald <ray_ex_59@hotmail.com>
2017-11-30 15:46:09 +0100

    correction syntax

--- src/main/java/fr/afpa/filRouge/service/IserviceAdministrator.java
+++ src/main/java/fr/afpa/filRouge/service/IserviceAdministrator.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import fr.afpa.filRouge.model.Administrator;
 
 /**
- * @author PH GUIGUE
+ * @author RTI
  *
  */
 @Service
--- src/main/java/fr/afpa/filRouge/service/IserviceInterest.java
+++ src/main/java/fr/afpa/filRouge/service/IserviceInterest.java
@@ -6,13 +6,13 @@ import fr.afpa.filRouge.model.Interest;
 
 public interface IserviceInterest {
 
-	 public List<Interest> getAll();
+	public List<Interest> getAll();
 
-	    public Interest getOne(String nameArea);
+	public Interest getOne(String nameArea);
 
-	    public void modifiedOne(Interest interest);
+	public void modifiedOne(Interest interest);
 
-	    public void addInterest(Interest interest);
+	public void addInterest(Interest interest);
 
-	    public void delInterest(Interest interest);
+	public void delInterest(Interest interest);
 }
