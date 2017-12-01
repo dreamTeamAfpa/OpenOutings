c92f03dd0f0eea63bdc3b36ae2ee00f1b628ed3b
Franck Descombes <franckdescombes@protonmail.com>
2017-12-01 10:16:28 +0100

    IServicePerson & ServicePerson & Person implemented Location & ServiceLocation modified

--- src/main/java/fr/afpa/filRouge/service/ServicePerson.java
+++ src/main/java/fr/afpa/filRouge/service/ServicePerson.java
@@ -3,10 +3,48 @@
  */
 package fr.afpa.filRouge.service;
 
+import java.util.List;
+
+import fr.afpa.filRouge.model.Person;
+import fr.afpa.filRouge.repository.PersonRepository;
+
 /**
- * @author PH GUIGUE
+ * @author FR DESCOMBES
  *
  */
 public class ServicePerson implements IservicePerson {
+
+	private PersonRepository personRepository;
+
+	public ServicePerson(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
+
+	@Override
+	public List<Person> getAll() {
+		return personRepository.findAll();
+	}
+
+	@Override
+	public Person getOne(int idUser) {
+		return personRepository.findOne(idUser);
+	}
+
+	@Override
+	public void modifiedPerson(Person person) {
+		personRepository.save(person);
+
+	}
+
+	@Override
+	public void addPerson(Person person) {
+		personRepository.save(person);
+	}
+
+	@Override
+	public void delPerson(Person person) {
+		personRepository.delete(person);
+
+	}
 
 }
