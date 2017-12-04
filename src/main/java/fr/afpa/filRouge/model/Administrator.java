add6df9b6e54b5f13b1e886c5a0f80475ee6e12f
TITECA Raynald <ray_ex_59@hotmail.com>
2017-12-04 09:38:04 +0100

    modif administrator

--- src/main/java/fr/afpa/filRouge/model/Administrator.java
+++ src/main/java/fr/afpa/filRouge/model/Administrator.java
@@ -1,6 +1,7 @@
 package fr.afpa.filRouge.model;
 
-import java.util.Calendar;
+
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -10,13 +11,10 @@ import javax.persistence.Table;
 @Entity
 @Table(name="administrator")
 public class Administrator extends Person{
-	
 
-	public Administrator(int idUser, String pseudoUser, String passwordUser, String firstNameUser, String lastNameUser,
-			Calendar dobUser, char genderUser, String emailUser, int phoneUser) {
-		super(idUser, pseudoUser, passwordUser, firstNameUser, lastNameUser, dobUser, genderUser, emailUser, phoneUser);
-		// TODO Auto-generated constructor stub
-	}
+@ManyToMany
+private List<Groupe> groupes;
+
 
 
 
