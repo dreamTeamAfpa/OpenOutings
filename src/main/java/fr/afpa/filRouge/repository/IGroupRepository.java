ea5d0007b4692892120d9d0f7f16c0398cf67861
Yannick Le Bihan <yannick-le-bihan@orange.fr>
2017-12-06 16:03:27 +0100

    maj repository create

--- src/main/java/fr/afpa/filRouge/repository/IGroupRepository.java
+++ src/main/java/fr/afpa/filRouge/repository/IGroupRepository.java
@@ -1,9 +1,10 @@
 package fr.afpa.filRouge.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import fr.afpa.filRouge.model.Groupe;
-
+@Repository
 public interface IGroupRepository extends JpaRepository<Groupe, Integer> {
 
 }
