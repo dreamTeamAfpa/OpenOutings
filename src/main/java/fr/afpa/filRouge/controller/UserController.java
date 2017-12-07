7306e08be9b2d3427daa4754ee2af50d9c40d46d
TITECA Raynald <ray_ex_59@hotmail.com>
2017-12-07 10:54:02 +0100

    maj controller user

--- src/main/java/fr/afpa/filRouge/controller/UserController.java
+++ src/main/java/fr/afpa/filRouge/controller/UserController.java
@@ -1,5 +1,24 @@
 package fr.afpa.filRouge.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class UserController {
+	
+	//affiche page signUp
+	@GetMapping("signUp")
+	public String signUp(Model model) {
+		return "sign_up";
+	}
+	
+	//affiche page signIn
+	@GetMapping("signIn")
+	public String signIn(Model model) {
+		return "sign_in";
+	}
 
 }
